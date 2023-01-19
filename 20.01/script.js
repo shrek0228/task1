70f9aa057dc91286f86bfac6b9ea175ead85c856
Serj <sergeyroman26@gmail.com>
2023-01-19 22:18:49 +0000

    20.01 Task1

---
+++ 20.01/script.js
@@ -0,0 +1,13 @@
+function updateTime() {
+    // Здесь должен быть элемент hours 
+    // Здесь должен быть элемент minutes 
+    // Здесь должен быть элемент seconds
+
+    const clock = new Date();
+
+    // getHours()
+    // getMinutes()
+    // getSeconds()
+
+}
+// Здесь должен быть задан интервал
