2520c391006b5421baeffe90a28e9a893d9e2491
Daniil <makh.daniil@gmail.com>
2023-01-20 22:31:25 +0200

    task1 Danya

--- 20.01/script.js
+++ 20.01/script.js
@@ -2,12 +2,17 @@ function updateTime() {
     // Здесь должен быть элемент hours 
     // Здесь должен быть элемент minutes 
     // Здесь должен быть элемент seconds
-
+    
     const clock = new Date();
-
+    var h = clock.getHours();
+    var m = clock.getMinutes();
+    var s = clock.getSeconds();
     // getHours()
     // getMinutes()
     // getSeconds()
-
+    document.getElementsByClassName('seconds')[0].innerHTML = s;
+    document.getElementsByClassName('minutes')[0].innerHTML = m;
+    document.getElementsByClassName('hours')[0].innerHTML = h;
 }
+setInterval(updateTime, 1000)
 // Здесь должен быть задан интервал
